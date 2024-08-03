a15a1c0319687673a749480cf5a0ef2a260734fc
CAHTA123 <135743906+CAHTA123@users.noreply.github.com>
2024-08-03 10:18:15 +0500

    .

--- scenes/Spawner/spawner.gd
+++ scenes/Spawner/spawner.gd
@@ -1,20 +1,31 @@
 extends Node2D
 
-var spawn
+@onready var spawn_area = get_node("Area2D")
+var spawn = "res://scenes/Enemy/enemy.tscn"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	spawn_all()
 	
 func spawn_all():
 	if spawn:
-		var pos 
+		var pos = random_point()
 		var s = load(spawn).instantiate()
-		s.global_position = pos.global_position
+		s.global_position = pos
 		get_parent().add_child(s)
+
+func random_point():
+	var shape = spawn_area.get_node("CollisionShape2D").get_shape() 
+	var circle_shape = shape as CircleShape2D # Получаем форму круга
+
+	# Генерируем случайные координаты внутри круга
+	var angle = randf_range(0, 2 * PI)
+	var radius = circle_shape.radius * randf_range(0, 1)
+	var x = radius * cos(angle) 
+	var y = radius * sin(angle) / 4 * 3
+	var point = Vector2(x, y)
+
+	# Переводим точку в глобальные координаты
+	return spawn_area.to_global(point) 
