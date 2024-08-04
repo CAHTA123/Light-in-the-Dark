b792efeb9fbf0dc4ef801f2d1c1a5800cef78979
CAHTA123 <135743906+CAHTA123@users.noreply.github.com>
2024-08-04 19:02:48 +0500

    .

--- scenes/Player/Move.gd
+++ scenes/Player/Move.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 @onready var b = $".."
 @onready var body = $"../Skin"
-@onready var skin = $"../Skin/Skin"
+@onready var skin1 = $"../Skin/Skin"
+@onready var skin2 = $"../Skin/Weapon"
 
 var current_state = St.l 
 enum St {l, r, t, d, ld, rd, lt, rt}
@@ -19,6 +20,7 @@ func move():
 	#var mouse_pos = get_global_mouse_position()
 	#var direction = (mouse_pos - body.global_position).normalized()
 	b.velocity = Vector2(Input.get_axis("A", "D"), Input.get_axis("W", "S")).normalized() * b.s
+	
 	if b.velocity.x > 0:
 		body.scale.x = -1
 	elif b.velocity.x < 0:
