1e9b4fd03c36cc673704cb9b105e1b470c79b9b8
CAHTA123 <135743906+CAHTA123@users.noreply.github.com>
2024-08-09 20:24:50 +0500

    .

--- scenes/inventory/inventory.gd
+++ scenes/inventory/inventory.gd
@@ -2,20 +2,20 @@ extends Node2D
 
 const SlotClass = preload("res://scenes/inventory/Slot.gd")
 @onready var inventory_slots = $GridContainer
-var holding_item: Node2D = null  # Указываем тип, если это Node2D
+var holding_item: Node2D = null
 
 func _ready():
 	for inv_slot in inventory_slots.get_children():
-		inv_slot.gui_input.connect(_on_slot_gui_input, [inv_slot])
+		inv_slot.gui_input.connect(_on_slot_gui_input, inv_slot)
 
 func _on_slot_gui_input(event: InputEvent, slot: SlotClass):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if holding_item != null:
-				if !slot.item: # Поместить удерживаемый предмет в слот
+				if !slot.item:  
 					slot.put_into_slot(holding_item)
 					holding_item = null
-				else: # Поменять удерживаемый предмет с предметом в слоте
+				else: 
 					var temp_item = slot.item
 					slot.pick_from_slot()
 					temp_item.global_position = event.global_position
