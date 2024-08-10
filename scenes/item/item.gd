0f0aeac19291dff8e766f1e7fd7d968c93cda9bc
CAHTA123 <135743906+CAHTA123@users.noreply.github.com>
2024-08-10 12:28:03 +0500

    .

--- scenes/item/item.gd
+++ scenes/item/item.gd
@@ -1,13 +1,8 @@
 extends Node2D
 
-
-
 func _ready():
 	if randi() % 2 == 0:
 		$TextureRect.texture = load("res://sprites/weapon/axe/A3.png")
 	else:
 		$TextureRect.texture = load("res://sprites/weapon/axe/A2.png")
 		
-
-
-
