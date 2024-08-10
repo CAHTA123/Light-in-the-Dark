c00e0d43f8aa084b2a44015affd15e842c8d28fc
Gravitydevl <andreilo5673@gmail.com>
2024-08-10 17:40:40 +0300

    .

--- scenes/Player/AnimPlayer.gd
+++ scenes/Player/AnimPlayer.gd
@@ -6,7 +6,9 @@ extends Node2D
 func _ready():
 	pass 
 
+
 func _on_animation_animation_finished(anim_name):
+	
 	if anim_name != "Idle":
 		body.current_state = body.States.IDLE
 		anim.play("Idle")
