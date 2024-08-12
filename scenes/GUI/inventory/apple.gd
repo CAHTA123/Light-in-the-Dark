62539c1d23c23d3be35e172cd2719ee0e5718f1c
CAHTA123 <135743906+CAHTA123@users.noreply.github.com>
2024-08-12 14:12:52 +0500

    .

--- scenes/GUI/inventory/apple.gd
+++ scenes/GUI/inventory/apple.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 @export var inv: Inv
 @export var item: InvItem
-func _on_detector_body_entered(_body):
-	print("dv")
+
+func _on_detector_body_entered(body):
+	print("1")
 	inv.insert(item)
 	self.queue_free()
