c2e23279e6955fafe3231b6a2b39e412923aa57e
CAHTA123 <135743906+CAHTA123@users.noreply.github.com>
2024-08-13 15:28:18 +0500

    .

--- scenes/Entities/Player/Check_inv.gd
+++ scenes/Entities/Player/Check_inv.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 @onready var body = $"../.."
 
-func _ready():
-	new_slots()
-
-
-
-func new_slots():
-	body.weapon
-	body.pickaxe
-	body.axe
+#func _ready():
+	#new_slots()
+#
+#
+#
+#func new_slots():
+	#body.weapon
+	#body.pickaxe
+	#body.axe
