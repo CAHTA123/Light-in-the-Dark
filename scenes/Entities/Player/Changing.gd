7e1a7ff64bd85a470e2d9c0781c16c0a626f348f
CAHTA123 <135743906+CAHTA123@users.noreply.github.com>
2024-08-15 13:04:11 +0500

    .

--- scenes/Entities/Player/Changing.gd
+++ scenes/Entities/Player/Changing.gd
@@ -3,15 +3,16 @@ extends Node2D
 @onready var _1 = $"../../Pause/Control/1"
 @onready var _2 = $"../../Pause/Control/2"
 @onready var _3 = $"../../Pause/Control/3"
+
 @onready var skin = $"../../Skin/Skin/Weapon/shape/Skin"
 @onready var b = $"../.."
 
 var now_slot
 
-func _ready():
-	check()
-
-
-func check():
-	
-	skin.texture = now_slot.texture
+#func _ready():
+	#check()
+#
+#
+#func check():
+	#
+	#skin.texture = now_slot.texture
--- scenes/Entities/Player/Input.gd
+++ scenes/Entities/Player/Input.gd
@@ -1,15 +1,10 @@
 extends CanvasLayer
 
-@onready var intv = $"."
-
 func _ready():
-	intv.visible = false
-
+	visible = false
 
 func _on_убрать_pressed():
-	intv.visible = false
-	
-
+	visible = false
 
 func _on_вернуть_pressed():
-	intv.visible = true
+	visible = true
