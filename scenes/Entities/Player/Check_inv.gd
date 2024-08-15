d2ba21c07e00f7a9e1be80a0be5e061a56ac53a9
CAHTA123 <135743906+CAHTA123@users.noreply.github.com>
2024-08-15 14:32:50 +0500

    /

--- scenes/Entities/Player/Check_inv.gd
+++ scenes/Entities/Player/Check_inv.gd
@@ -2,38 +2,82 @@ extends Node2D
 
 @onready var body = $"../.."
 
-func _ready():
+@onready var _1 = $"../../Pause/Control/1"
+@onready var _2 = $"../../Pause/Control/2"
+@onready var _3 = $"../../Pause/Control/3"
+
+@onready var skin = $"../../Skin/Skin/Weapon/shape/Skin"
+
+
+var cur = 0
+var item_on = []
+func _ready():	
+	item_on.append(body.weapon.i.name)
+	item_on.append(body.axe.i.name)
+	item_on.append(body.pickaxe.i.name)
+	
+	body.now_slot = item_on[cur]
 	new_slots()
 
 func _process(delta):
-	if body.new_item:
-		body.new_item = false
+
+	if Input.is_action_just_pressed("X"):
+		cur = (cur + 1) % item_on.size()
+		
+		body.now_slot = item_on[cur]
 		new_slots()
 
+	elif Input.is_action_just_pressed("Z"):
+		cur = (cur - 1 + item_on.size()) % item_on.size()
+		body.now_slot = item_on[cur]
+		new_slots()
+	
+	#if s_axe != body.axe.i.name:
+		#s_axe = body.axe.i.name
+		#new_slots()
+	#if s_shield != body.shield.i.name:
+		#s_shield = body.shield.i.name
+		#new_slots()
+	#if s_pickaxe != body.pickaxe.i.name:
+		#s_pickaxe = body.pickaxe.i.name
+		#new_slots()
+	#if s_weapon != body.weapon.i.name:
+		#s_weapon = body.weapon.i.name
+		#new_slots()
+	
+
 func new_slots():
-	if body.weapon.i:
-		body.weapon_tex = body.weapon.i.texture
-		$"../../Skin/Skin/Weapon/shape/Skin".texture = body.weapon.i.texture
+	if body.weapon.i and body.now_slot == item_on[0]:
+		skin.texture = body.weapon.i.texture
 		for key in body.weapon.i.character.keys():
 			if key in body:
 				body[key] = body.weapon.i.character[key]
+	else:
+		for key in body.weapon.i.character.keys():
+			if key in body:
+				body[key] = 0
 
-	if body.pickaxe.i:
-		body.pickaxe_tex = body.pickaxe.i.texture
-		$"../../Skin/Skin/Tool/Area2D/Skin".texture = body.weapon.i.texture
+	if body.pickaxe.i and body.now_slot == item_on[1]:
+		skin.texture = body.pickaxe.i.texture
 		for key in body.pickaxe.i.character.keys():
 			if key in body:
 				body[key] = body.pickaxe.i.character[key]
+	else:
+		for key in body.pickaxe.i.character.keys():
+			if key in body:
+				body[key] = 0
 
-	if body.axe.i:
-		body.axe_tex = body.axe.i.texture
-		$"../../Skin/Skin/Tool/Area2D/Skin".texture = body.weapon.i.texture
+	if body.axe.i and body.now_slot == item_on[2]:
+		skin.texture = body.axe.i.texture
 		for key in body.axe.i.character.keys():
 			if key in body:
 				body[key] = body.axe.i.character[key]
+	else:
+		for key in body.axe.i.character.keys():
+			if key in body:
+				body[key] = 0
 
 	if body.shield.i:
-		body.shield_tex = body.shield.i.texture
 		$"../../Skin/Skin/Shield/Skin".texture = body.shield.i.texture
 		for key in body.shield.i.character.keys():
 			if key in body:
--- scenes/weapon/weapon.gd
+++ scenes/weapon/weapon.gd
@@ -3,4 +3,9 @@ extends Node2D
 @onready var b = $"../../.."
 
 func _on_shape_body_entered(body):
-	body.take_damage = b.damage
+	if b.damage > 0:
+		body.take_damage = b.damage
+	if b.adamage > 0:
+		body.take_damage = b.adamage
+	if b.pdamage > 0:
+		body.take_damage = b.pdamage
