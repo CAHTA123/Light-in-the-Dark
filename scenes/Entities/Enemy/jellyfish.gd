d4f3155e43fb075c5bab59c02e2b181fff041e76
LemonDemon-ai <leon.gladunov@bk.ru>
2024-08-21 21:01:32 +0400

    Fix jelyfish, Update kamikadze

--- scenes/Entities/Enemy/jellyfish.gd
+++ scenes/Entities/Enemy/jellyfish.gd
@@ -12,35 +12,34 @@ func _ready() -> void:
 	timer.set_wait_time(0.01)
 	timer.set_one_shot(false)
 	timer.stop()
-
-
 func drop():
 	pass
-
-
-
+#Получение урона
 func _on_hurt_area_entered(area: Area2D) -> void:
 	if hp <= player.damage:
 		drop()
 		queue_free()
 	else:
 		hp -= player.damage
-
-
+#обнаружение игрока в области преследования
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.get_name() == "Player":
 		player = body
 		$HuntTimer.start()
+		
+#Обнаружение выхода игрока из зоны преследования
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.get_name() == "Player":
 		player = null
 		$HuntTimer.stop()
 		regen_hp()
+#Регенерация хп
 func regen_hp():
 	hp += 1
 	await get_tree().create_timer(5).timeout
 	if not player:
 		regen_hp()
+#Преследование
 func hunt():
 	if hp > 0 and not attack:
 		var direction = (player.global_position - global_position)
@@ -58,7 +57,7 @@ func hunt():
 func _on_timer_timeout():
 	if player != null:
 		hunt()
-		
+#Рандомная ходьба в разные стороны
 func random_walk():
 	while true:
 		velocity.x = randi_range(-50, 50)
@@ -68,12 +67,12 @@ func random_walk():
 			#$"Body_Skin#Skin#Skin/Skin".scale.x = -1
 		velocity.y = randi_range(-50, 50)
 		await get_tree().create_timer(5).timeout
-
-
+#Обнаружение игрока в зоне атаки
 func _on_attack_direction_body_entered(body: Node2D) -> void:
 	if body.get_name() == "Player":
 		await get_tree().create_timer(2).timeout
 		$Anim/AnimationPlayer.play("attack")
+#Обнаружение игрока в хитбоксе атаки
 func _on_attack_hitbox_body_entered(body: Node2D) -> void:
 	if body.get_name() == "Player":
 		body.hp -= 2
